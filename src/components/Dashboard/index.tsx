3dd663da7daf9020dc3ff96aca325e598268d5ed
CodeAkio <siqueira_2004@hotmail.com>
2021-04-08 20:56:55 -0300

    Summary layout

---
+++ src/components/Dashboard/index.tsx
@@ -0,0 +1,12 @@
+import { Container } from './styles';
+import Summary from '../Summary';
+
+const Dashboard = () => {
+  return (
+    <Container>
+      <Summary />
+    </Container>
+  );
+}
+
+export default Dashboard;
