3ade689875f7dc2bd7bbf78878e753dc646be85f
CodeAkio <siqueira_2004@hotmail.com>
2021-04-09 16:31:01 -0300

    Get new transaction form data

--- src/components/NewTtansactionModal/index.tsx
+++ src/components/NewTtansactionModal/index.tsx
@@ -1,3 +1,4 @@
+import { FormEvent, useState } from 'react';
 import Modal from 'react-modal';
 
 import incomeImg from '../../assets/income.svg';
@@ -5,7 +6,6 @@ import outcomeImg from '../../assets/outcome.svg';
 import closeImg from '../../assets/close.svg';
 
 import { Container, TransactionTypeContainer, RadioBox } from './styles';
-import { useState } from 'react';
 
 interface NewTtansactionModalProps {
   isOpen: boolean;
@@ -13,7 +13,16 @@ interface NewTtansactionModalProps {
 }
 
 export function NewTtansactionModal({ isOpen, onRequestClose }: NewTtansactionModalProps) {
+  const [title, setTitle] = useState('');
+  const [value, setValue] = useState(0.0);
+  const [category, setCategory] = useState('');
   const [type, setType] = useState('deposit');
+
+  function handleCreateNewTransaction(event: FormEvent) {
+    event.preventDefault();
+
+
+  }
 
   return (
     <Modal
@@ -30,14 +39,18 @@ export function NewTtansactionModal({ isOpen, onRequestClose }: NewTtansactionMo
         <img src={closeImg} alt="Fechar modal"/>
       </button>
 
-      <Container>
+      <Container onSubmit={handleCreateNewTransaction}>
         <h2>Cadastrar transação</h2>
 
         <input
+          value={title}
+          onChange={event => setTitle(event.target.value)}
           placeholder="Título"
         />
 
         <input
+          value={value}
+          onChange={event => setValue(Number(event.target.value))}
           type="number"
           placeholder="Valor"
         />
@@ -65,6 +78,8 @@ export function NewTtansactionModal({ isOpen, onRequestClose }: NewTtansactionMo
         </TransactionTypeContainer>
 
         <input
+          value={category}
+          onChange={event => setCategory(event.target.value)}
           placeholder="Categoria"
         />
 
