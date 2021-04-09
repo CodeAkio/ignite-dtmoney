ded9ecbd466081f36f61b867f2f4ac7b0bf83551
CodeAkio <siqueira_2004@hotmail.com>
2021-04-08 21:16:16 -0300

    transactions table layout

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,10 +1,12 @@
 import { Container } from './styles';
 import Summary from '../Summary';
+import TransactionsTable from '../TransactionsTable';
 
 const Dashboard = () => {
   return (
     <Container>
       <Summary />
+      <TransactionsTable />
     </Container>
   );
 }
