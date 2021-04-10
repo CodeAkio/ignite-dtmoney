f4b44371b7feed4f8f47b21c7e5db9b2f8af58ea
CodeAkio <siqueira_2004@hotmail.com>
2021-04-09 21:20:54 -0300

    Calc summary

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -3,11 +3,28 @@ import incomeImg from '../../assets/income.svg';
 import outcomeImg from '../../assets/outcome.svg';
 import totalImg from '../../assets/total.svg';
 import { TransactionsContext } from '../../context/TransactionsContext';
+import { formatPrice } from '../../util/format';
 
 import { Container } from './styles';
 
 const Summary: React.FC = () => {
   const { transactions } = useContext(TransactionsContext);
+
+  const summary = transactions.reduce((acc, transaction) => {
+    if (transaction.type === 'deposit') {
+      acc.deposits += transaction.amount;
+      acc.total += transaction.amount;
+    } else {
+      acc.withdraws += transaction.amount;
+      acc.total -= transaction.amount;
+    }
+
+    return acc;
+  }, {
+    deposits: 0,
+    withdraws: 0,
+    total: 0,
+  });
 
   return (
     <Container>
@@ -16,21 +33,21 @@ const Summary: React.FC = () => {
           <p>Entradas</p>
           <img src={incomeImg} alt="Entradas"/>
         </header>
-        <strong>R$1000,00</strong>
+        <strong>{formatPrice(summary.deposits)}</strong>
       </div>
       <div>
         <header>
           <p>Saídas</p>
           <img src={outcomeImg} alt="Saídas"/>
         </header>
-        <strong>- R$500,00</strong>
+        <strong>{formatPrice(summary.withdraws)}</strong>
       </div>
       <div className="highlight-background">
         <header>
           <p>Total</p>
           <img src={totalImg} alt="Total"/>
         </header>
-        <strong>R$500,00</strong>
+        <strong>{formatPrice(summary.total)}</strong>
       </div>
     </Container>
   );
